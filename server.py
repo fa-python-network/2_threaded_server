1898daa0353ac071dfbbdf83b08cda4a1319f8f7
Степанов Игорь Евгеньевич <181418@edu.fa.ru>
2019-10-08 10:50:29 +0300

    2. Модифицировать код сервера таким образом, чтобы при подключении нового клиента создавался новый поток и вся работа с клиентом выполнялась в нем.

--- server.py
+++ server.py
@@ -1,5 +1,14 @@
 import socket
+from threading import Thread
 
+def one_client(conn,addr):
+	print("Стартовал новый поток")
+	data = conn.recv(1024)
+	if not data:
+		conn.close()
+		
+	print(data.decode())
+	conn.send("Hello from server!".encode())
 
 sock = socket.socket()
 
@@ -11,10 +20,6 @@ sock.listen(3)
 while True:
 	conn, addr = sock.accept()
 	
-	data = conn.recv(1024)
-	if not data:
-		conn.close()
-		
-	print(data.decode())
-	conn.send("Hello from server!".encode())
+	print("Новый клиент!")
+	Thread(target=one_client,args=(conn,addr)).start()
 	
