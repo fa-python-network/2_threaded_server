3e6ca4ab1967f034e2b8b40ac7b1cba9bc049617
Skorpx <sdn2000@bk.ru>
2019-10-10 19:49:07 +0300

    1-3 general tasks (Not fixed)

--- client.py
+++ client.py
@@ -1,2 +1,17 @@
+
 import socket
-import threading
+from time import sleep
+SERVER = "127.0.0.1"
+PORT = 8080
+client = socket.socket()
+client.setblocking(1)
+client.connect((SERVER, PORT))
+# client.sendall(bytes("This is from Client",'UTF-8'))
+while True:
+	# in_data = client.recv(1024)
+	# print("From Server :" ,in_data.decode())
+	out_data = input()
+	client.send(out_data.encode())
+	if out_data=='bye':
+		break
+client.close()
