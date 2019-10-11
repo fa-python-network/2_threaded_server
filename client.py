4b808a431ad93b9f9a87a72616e87afc2cace54b
StepIgor <45178763+StepIgor@users.noreply.github.com>
2019-10-11 11:41:19 +0300

    2 ЗАДАНИЕ почти готово

--- client.py
+++ client.py
@@ -1,4 +1,10 @@
 import socket
+from threading import Thread
+
+def update_chat():
+	while True:
+		answer = sock.recv(1024)
+		print(f'\n',answer.decode())
 
 ip = 'localhost'
 port = 9100
@@ -7,16 +13,41 @@ sock = socket.socket()
 
 sock.connect((ip,port))
 
-while True:
-	sock.send("Hello from client!".encode())
+print('Подключились к серверу чата')
 
-	data = sock.recv(1024)
-	print(data.decode())
-	
-	action = input()
-	
-	if action == "exit":
-		sock.send(action.encode())
+username = ""
+password = ""
+
+while not username:
+	username = input('Введите Ваше имя: ')
+
+sock.send(username.encode())
+answer = sock.recv(1024)
+
+if answer.decode() == 'reg_req':
+	#registation required
+	print('Такого пользователя ещё нет! Запускается процесс регистрации!')
+	print(f'Регистрируемое имя: {username}')
+	while not password:
+		password = input('Придумайте пароль:')
+	sock.send(password.encode())
+	answer = sock.recv(1024)
+	if answer.decode() == 'reg_end':
+		print('Регистрация завершена! Перезапустите программу!')
+else:
+	password = input('Ваш пароль от аккаунта: ')
+	sock.send(password.encode())
+	answer = sock.recv(1024)
+	if answer.decode() == 'valid':
+		Thread(target=update_chat,args=()).start()
+		while True:
+			msg = input('Ваше сообщение: ')
+			if msg == 'exit':
+				sock.send('exit_cmd'.encode())
+				break
+			sock.send(msg.encode())
+	else:
+		print('Введен неверный пароль!')
 
 print("Подключение закрыто!")
 sock.close()
--- server.py
+++ server.py
@@ -1,18 +1,67 @@
 import socket
 from threading import Thread
+import pickle
+import os
+
 
 def one_client(conn,addr):
+	global connected_users
+	connected_users.append(conn)
 	print("Стартовал новый поток")
 	while True:
-		data = conn.recv(1024)
-		if data.decode() == "exit":
-			print("Один клиент отключился!")
+		username = conn.recv(1024)
+		if username.decode() in db.keys():
+			password = db[username.decode()]
+			conn.send('ask_pass'.encode())
+			userpassword = conn.recv(1024)
+			if password == userpassword.decode():
+				conn.send('valid'.encode())
+				while True:
+					msg = conn.recv(1024)
+					if msg.decode() == 'exit_cmd':
+						msg = f'{username} покинул этот чат.'
+						connected_users.remove(conn)
+						conn.close()
+						for user in connected_users:
+							if user != conn:
+								user.send(msg.encode())
+						break
+					msgpart1 = f'[{username.decode()}]: '
+					msgpart2 = msg.decode()
+					msgsend = msgpart1 + msgpart2
+					for user in connected_users:
+						if user != conn:
+							user.send(msgsend.encode())
+				break
+			else:
+				conn.send('error'.encode())
+				conn.close()
+				break
+		else:
+			#registration required
+			conn.send('reg_req'.encode())
+			password = conn.recv(1024)
+			db.update({username.decode():password.decode()})
+			with open(db_file,'wb') as f:
+				pickle.dump(db,f)
+			conn.send('reg_end'.encode())
 			conn.close()
-			
-		print(data.decode())
-		conn.send("Hello from server!".encode())
+			break
 
 sock = socket.socket()
+
+db_file = 'clients.pickle'
+db = {}
+connected_users = []
+
+#читаем базу
+if os.path.getsize(db_file) > 0:
+	with open(db_file,'rb') as f:
+		db = pickle.load(f)
+else:
+	db = {}
+		
+print(f'База клиентов и их пароли:\n',db)
 
 port = 9100
 
