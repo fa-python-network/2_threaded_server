17ea3998b58273eeb2d4733106066f85daed854c
Дмитрий Гриднев <flymedllva@gmail.com>
2019-10-11 09:42:19 +0300

    Все задания, кроме сканера TCP-портов

---
+++ server.py
@@ -0,0 +1,13 @@
+from server import Server
+
+if __name__ == '__main__':
+
+    server = Server(port=9090)
+
+    # server.start()
+    # server.start_interactive()
+
+    server.interactive = True
+    server.start()
+
+    # TODO: Поймать KeyboardInterrupt
