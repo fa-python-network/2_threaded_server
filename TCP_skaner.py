45bcf00153815782c934951da92478c24acc99ef
LiliyaRachkova <lili.rachkova@ya.ru>
2019-10-16 20:59:49 +0300

    сканер c потоками

--- TCP_skaner.py
+++ TCP_skaner.py
@@ -1,17 +1,21 @@
 import socket
+import threading
+
 def new_sock(port, host):
     sock = socket.socket()
     try:
         sock.connect((host, port))
         print(f"Порт {port} свободен")
     except OSError:
-        print(f"Порт {port} занят")
+        pass
+        #print(f"Порт {port} занят")
     sock.close()
+
 
 print("Host:")
 host = input()
 if host == "":
     host = 'localhost'
 
-for i in range(1,65536):
-    new_sock(i, host)
+for i in range(1, 65536):
+    threading.Thread(target=new_sock, args=[i, host]).start()
