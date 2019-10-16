b8d98a7e3bf67fcb4b3ad80065520aece867f52a
nastya <pin-2169@rambler.ru>
2019-10-16 20:19:52 +0300

    Threads - 3rd task

--- client.py
+++ client.py
@@ -1,60 +1,91 @@
-from threading import Thread 
-import tkinter
 import socket
+import threading
 
-def receive():
-	"""Receive message"""
-	while True:
-		try:
-			msg = client_socket.recv(1024).decode()
-			msg_list.insert(tkinter.END, msg)
-		except OSError:
-			break
+class Client(socket.socket):
 
-def send(event=None):
-	msg = my_msg.get()
-	my_msg.set("") 
-	client_socket.send(msg.encode())
-	if msg == "{quit}":
-		client_socket.close()
-		top.quit()
+	def __init__(self):
+		self.sock = socket.socket()
+		self.lock = threading.Lock()
 
-def on_closing(event=None):
-	my_msg.set("{quit}")
-	send()
+	@property
+	def sending(msg, sock):
+		sock.send(msg.encode())
 
-top = tkinter.Tk()
-top.title("Chatting room")
+	@property 
+	def receiving(sock):
+		sock.recv(1024).decode()
 
-messages_frame = tkinter.Frame(top)
-my_msg = tkinter.StringVar()
-my_msg.set("Type your message")
-scrollbar = tkinter.Scrollbar(messages_frame)
+	def register(self, sock):
+		name = input()
+		self.sending(name, sock)
+		password = input()
+		self.sending(password, sock)
 
-msg_list=tkinter.Listbox(messages_frame, height=40, width=100, yscrollcommand=scrollbar.set)
-scrollbar.pack(side=tkinter.RIGHT, fill = tkinter.Y)
-msg_list.pack(side=tkinter.LEFT, fill = tkinter.BOTH)
-msg_list.pack()
-messages_frame.pack()
+	def login(self,sock, msg):
+		print(msg)
+		password = input()
+		self.sending(password, sock)
+		mes = self.receiving(sock)
+		if mes == "Welcome":
+			pass
+		else:
+			self.login(sock, "Input correct password")
 
-entry_field = tkinter.Entry(top, textvariable = my_msg)
-entry_field.bind("<Return>", send)
-entry_field.pack()
-send_button = tkinter.Button(top, text="Send", command=send)
-send_button.pack()
+	def connection(self):
+		self.host = input("Input host: ")
+		port= input("Input port: ")
+		if self.host=="":
+			self.host = "127.0.0.1"
+		if port == "":
+			self.port=9090
+		else:
+			self.port=int(port)
+		self.sock.connect((self.host, self.port))
+		print("Connection")
 
-top.protocol("WM_DELETE_WINDOW", on_closing)
+	def hi(self):
+		data = self.receiving(self.sock)
+		if data == "Register":
+			self.register(self.sock)
+		elif data == "Login":
+			self.login(self.sock, "Input passwd")
+		data = self.receiving(self.sock)
+		print(data)
 
-host="127.0.0.1"
-port=7072
+	def chatting(self):
+		print("Input quit if you want to exit")
+		send1 = threading.Thread(target=self.send_msg_to_chat)
+		recv1 = threading.Thread(target=self.recv_msg_from_chat)
 
-client_socket=socket.socket()
-client_socket.connect((host, port))
+		send1.start()
+		recv1.start()
 
-receive_thread = Thread(target=receive)
-receive_thread.start()
-tkinter.mainloop()
+	def recv_msg_from_chat(self):
+		while True:
+			data = self.receiving(self.sock)
+			if not data:
+				continue
+			else:
+				print(data)
 
+	def send_msg_to_chat(self):
+		with self.lock:
+			msg=input()
+		while msg!="quit":
+			self.sending(msg, self.sock)
+			msg=input()
+
+	def closing(self):
+		self.sock.close()
+		print("Stop connecting")
+
+	def run(self):
+		self.connection()
+		self.hi()
+		self.chatting()
+
+sock = Client()
+sock.run()
 
 
 
--- server.py
+++ server.py
@@ -1,52 +1,146 @@
 import socket
-from threading import Thread 
+import sys
+import logging
+import threading
 
-def accept_incoming_connections():
-	""" Set up new clients """
-	while True:
-		client, client_address = server.accept()
-		print("{} client has connected".format(client_address))
-		client.send("Welcome everyone! Join the chat! Please, type your nickname so other chat users know, who sends messages".encode())
-		addresses[client] = client_address
-		Thread(target = handle_client, args=(client,)).start()
+log_file = logging.FileHandler('myserver.log')
+console_out= logging.StreamHandler()
 
-def handle_client(client):
-	name = client.recv(1024).decode()
-	welcome = "Welcome %s! If you want to quit, print {quit} to exit" %name
-	client.send(welcome.encode())
-	msg = "%s has joined the chat!" %name
-	broadcast(msg.encode())
-	clients[client] = name
+logging.basicConfig(handlers = (log_file, console_out), format = '[%(asctime)s | %(levelname)s] : %(message)s', datefmt='%m.%d.%Y %H:%M:%S', level = logging.INFO)
 
-	while True:
-		msg = client.recv(1024)
-		if msg!=bytes("{quit}", "utf8"):
-			broadcast(msg, name+": ")
+class Server(socket.socket):
+
+	def __init__(self):
+		self.sock = socket.socket()
+		self.port = 9090
+		self.host = '127.0.0.1'
+		self.threads=[]
+		self.lock = threading.Lock()
+		self.data=""
+
+	@property
+	def sending(msg, sock):
+		sock.send(msg.encode())
+
+	@property 
+	def receiving(sock):
+		sock.recv(1024).decode()
+
+	def close_server(self):
+		logging.info("Stop server")
+
+		#Очистка логов
+		with open('myserver.log', 'w'):
+			pass
+
+		self.sock.close()
+		sys.exit()
+
+		#Очистка файла идентификации
+		f = open('clients.txt', 'r+')
+		f.truncate()
+		
+
+	def register(self, conn, addr):
+		self.sending("Input your name")
+		name = self.receiving(conn)
+		self.sending("Input your password")
+		password = self.receiving(conn)
+		with open('clients.txt', 'w') as file:
+			file.write("{addr[0]}:{name}:{password}:\n")
+
+	def login(self, conn, addr):
+		with open('clients.txt', 'r+') as file:
+			for line in file:
+				if addr[0] in line:
+					line=line.split(":")
+					password = line[2]
+
+		self.sending("Type password")
+		passwd = self.receiving(conn)
+		if passwd == password:
+			conn.sending("Welcome", conn)
+			self.receiving(conn)
 		else:
-			client.send(bytes("{quit}", "utf8"))
-			client.close()
-			del clients[client]
-			broadcast("{} has left the chat".format(name))
-			break
+			conn.sending("Wrong password", conn)
+			self.login(conn, addr)
 
-def broadcast(msg, prefix=""):
-	"""For all users"""
-	for sock in clients:
-		sock.send(bytes(prefix, "utf8")+msg)
+	def bind(self):
+		while True:
+			try:
+				sock.bind(('', self.port))
+				break
+			except:
+				self.port+=1
+		logging.info("Server started")
 
-clients={}
-addresses={}
+	def listen(self, count):
+		self.sock.listen(count)
 
-host="127.0.0.1"
-port=7072
+		#пауза
+		socket.settimeout(0.1)
+		
+		logging.info("Server is listenning")
 
-server=socket.socket()
-server.bind((host, port))
+	def connecting(self):
+		thread = threading.Thread(target=self.connecting)
+		self.threads.append(thread)
 
-if __name__ == "__main__":
-	server.listen(2)
-	print("Waiting connection...")
-	accept_thread=Thread(target=accept_incoming_connections)
-	accept_thread.start()
-	accept_thread.join()
-	server.close()
+		conn, addr = self.sock.accept()
+		logging.info("Client connection")
+		thread.start()
+
+		self.hi(conn, addr)
+
+	def hi(self, conn, addr):
+		with open('clients.txt', 'r+') as file:
+			for line in file:
+				if addr[0] not in line:
+					self.sending("Register", conn)
+					self.register(conn, addr)
+				else:
+					self.sending("Login", conn)
+					self.login(conn, addr)
+
+		with open('clients.txt', 'r+') as file:
+			for line in file:
+				if addr[0] in line:
+					line=line.split(":")
+					name = line[1]
+
+		msg = "Hello " + name
+		self.sending(msg, conn)
+
+		self.chatting(conn)
+
+	def chatting(self, conn, addr):
+
+		send=threading.Thread(target=self.send_msg_to_chat, args=[conn])
+		recv = threading.Thread(target = self.recv_msg_from_chat, args=[conn, addr])
+
+		send.start()
+		recv.start()
+
+	def recv_msg_from_chat(self, conn, addr):
+		while True:
+			data = self.receiving(conn)
+			logging.info("Receive message")
+			self.data = self.data + data + '\n'
+
+	def send_msg_to_chat(self, conn):
+		while True:
+			with self.lock:
+				self.sending(self.data, conn)
+				logging.info("Send message to clients")
+
+sock = Server()
+
+try:
+	sock.bind()
+	print(port)
+	sock.listen(5)
+	while True:
+		sock.connecting()
+except:
+	sock.close_server()
+
