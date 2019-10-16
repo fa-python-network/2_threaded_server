ad26460a47de83130cc74ebf0dea7be0819389cf
LiliyaRachkova <lili.rachkova@ya.ru>
2019-10-16 21:19:23 +0300

    сортировка свободных портов

--- TCP_skaner.py
+++ TCP_skaner.py
@@ -1,11 +1,12 @@
 import socket
 import threading
 
-def new_sock(port, host):
+def new_sock(port, host, ports_list):
     sock = socket.socket()
     try:
         sock.connect((host, port))
-        print(f"Порт {port} свободен")
+        ports_list.append(port)
+        #print(f"Порт {port} свободен")
     except OSError:
         pass
         #print(f"Порт {port} занят")
@@ -14,8 +15,12 @@ def new_sock(port, host):
 
 print("Host:")
 host = input()
+ports_list = []
 if host == "":
     host = 'localhost'
 
 for i in range(1, 65536):
-    threading.Thread(target=new_sock, args=[i, host]).start()
+    threading.Thread(target=new_sock, args=[i, host, ports_list]).start()
+print("Список свободных портов")
+#не хотелось делать отдельный цикл на вывод
+print(sorted(ports_list))
