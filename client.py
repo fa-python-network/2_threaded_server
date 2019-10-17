60dab4ff275cd487b22d9e3b36f839bf3e0d27b6
Vlad <vlad07020@mail.ru>
2019-10-17 21:37:45 +0300

    2_zadanie

--- client.py
+++ client.py
@@ -1,7 +1,14 @@
 import socket
-sock = socket.socket()
-sock.connect(('localhost', 9090))
-sock.send('hello, world!')
-data = sock.recv(1024)
-sock.close()
-print(data)
+SERVER = "127.0.0.1"
+PORT = 8080
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((SERVER, PORT))
+client.sendall(bytes("Это клиенту",'UTF-8'))
+while True:
+  in_data =  client.recv(1024)
+  print("Для сервера :" ,in_data.decode())
+  out_data = input()
+  client.sendall(bytes(out_data,'UTF-8'))
+  if out_data=='exit':
+  	break
+client.close()
--- server.py
+++ server.py
@@ -1,11 +1,29 @@
-import socket
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(1)
-conn, addr = sock.accept()
+import socket, threading
+class ClientThread(threading.Thread):
+    def __init__(self,clientAddress,clientsocket):
+        threading.Thread.__init__(self)
+        self.csocket = clientsocket
+        print ("Новое соединение создано: ", clientAddress)
+    def run(self):
+        print ("Подключение : ", clientAddress)
+        msg = ''
+        while True:
+            data = self.csocket.recv(2048)
+            msg = data.decode()
+            if msg=='exit':
+              break
+            print ("От клиента: ", msg)
+            self.csocket.send(bytes(msg,'UTF-8'))
+        print ("Клиент ", clientAddress , " отсоеденился")
+LOCALHOST = "127.0.0.1"
+PORT = 8080
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((LOCALHOST, PORT))
+print("Сервер запущен")
+print("Ожидается подключение клиентов")
 while True:
-     data = conn.recv(1024)
-     if not data:
-         break
-     conn.send(data.upper())
-conn.close()
+    server.listen(1)
+    clientsock, clientAddress = server.accept()
+    newthread = ClientThread(clientAddress, clientsock)
+    newthread.start()
