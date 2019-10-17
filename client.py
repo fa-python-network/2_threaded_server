77529be078ade17a0c71eb7d68fbfd15367e99f2
Vlad <vlad07020@mail.ru>
2019-10-17 21:30:58 +0300

    1_zadanie

---
+++ client.py
@@ -0,0 +1,7 @@
+import socket
+sock = socket.socket()
+sock.connect(('localhost', 9090))
+sock.send('hello, world!')
+data = sock.recv(1024)
+sock.close()
+print(data)
---
+++ server.py
@@ -0,0 +1,11 @@
+import socket
+sock = socket.socket()
+sock.bind(('', 9090))
+sock.listen(1)
+conn, addr = sock.accept()
+while True:
+     data = conn.recv(1024)
+     if not data:
+         break
+     conn.send(data.upper())
+conn.close()
