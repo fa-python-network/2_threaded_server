ac939d93217067311261b43b191473af41672c0c
Vlad <vlad07020@mail.ru>
2019-10-17 21:42:16 +0300

    scaner_2

--- scaner.py
+++ scaner.py
@@ -1,7 +1,9 @@
+import threading
 import socket
 
 def scan_port(ip,port):
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  sock.settimeout(0.5)
   try:
      connect = sock.connect((ip,port))
      print('Порт с номером : ',port,' открыт')
@@ -10,5 +12,7 @@ def scan_port(ip,port):
      pass
 
 ip = '192.168.0.1'
+
 for i in range(49000):
-  scan_port(ip,i)
+ potoc = threading.Thread(target=scan_port, args=(ip,i))
+ potoc.start()
