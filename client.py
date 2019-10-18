225d1abe2073b2c4081e6ea0356aa6dfb93e9b83
root <vmolchanovag@mail.ru>
2019-10-19 00:47:53 +0300

    tasks 1,2,3

--- client.py
+++ client.py
@@ -24,6 +24,7 @@ while True:											# проверка порта
 sock = socket.socket()
 sock.setblocking(1)
 sock.connect((client_host,int(client_port)))
+print('Connected to server')
 
 def send_msg(conn: socket.socket, msg):
 	"""
@@ -41,33 +42,16 @@ def recv_msg(conn:socket.socket):
 	msg=conn.recv(int(header))
 	return msg
 
-def auten():
-	"""
-	аутентификация пользователя
-	"""
-	answer=recv_msg()
-	if 'Hello' in answer:
-		pswd_ans=recv_msg()
-		print('Password: ')
-		while True:
-			send_msg(input())
-			if 'Lets' in recv_msg():
-				print('Correct')
-				break
-			print('Again')
-	elif "Create" in answer:
-		print(answer)
-		send_msg(input('New Name: '))
-		print(recv_msg())
-		send_msg(input('New password: '))
-
+send_msg(sock, input('Enter your name, please: '))
 
 msg=''
 while True:
 	msg=input('Message: ')
+	print('Sending your message to server')
 	if 'exit' in msg:
 		send_msg(sock,msg)
 		break
 	send_msg(sock,msg)
+
 sock.close()
 
