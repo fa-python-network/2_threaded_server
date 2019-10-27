fb7413aa7954515c96c77e710c597ec9cae36604
Vlad <vlad07020@mail.ru>
2019-10-27 13:49:00 +0300

    Zadanie 1

--- server.py
+++ server.py
@@ -1,55 +1,15 @@
-iserver.py
+import socket
 
-import socket 
-import select 
-import sys 
-from thread import *
-  
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
- 
-if len(sys.argv) != 3: 
-    print "Неправильные данные"
-    exit() 
-IP_address = str(sys.argv[1]) 
-Port = int(sys.argv[2]) 
-server.bind((IP_address, Port)) 
-server.listen(100) 
-list_of_clients = []   
-def clientthread(conn, addr): 
-  
- 
-    conn.send("Всем кискам пис, всем ****** кис, вы в чате") 
-    while True: 
-            try: 
-                message = conn.recv(2048) 
-                if message: 
-                    print "<" + addr[0] + "> " + message 
-                    message_to_send = "<" + addr[0] + "> " + message 
-                    broadcast(message_to_send, conn) 
-                else: 
-                    remove(conn)  
-            except: 
-                continue
+sock=socket.socket()
 
-def broadcast(message, connection): 
-    for clients in list_of_clients: 
-        if clients!=connection: 
-            try: 
-                clients.send(message) 
-            except: 
-                clients.close() 
-                remove(clients) 
+sock.bind(("", 8082))
+sock.listen(1)
 
-def remove(connection): 
-    if connection in list_of_clients: 
-        list_of_clients.remove(connection) 
-  
-while True: 
-    conn, addr = server.accept() 
-    list_of_clients.append(conn) 
-    print addr[0] + " connected"
-    start_new_thread(clientthread,(conn,addr))     
-  
-conn.close() 
-server.close() 
+conn,addr = sock.accept()
+while True:
+	data = conn.recv(1024).decode()
+	if not data:
+		break
+	conn.send(data.encode())
+
+conn.close()
