831bae985e58865c1669b4b79af63b9533857c38
Vlad <vlad07020@mail.ru>
2019-10-27 13:52:30 +0300

    Dop zadanie 1

--- scaner.py
+++ scaner.py
@@ -1,55 +1,15 @@
-from queue import Queue
 import socket
-import threading
 
-target = "127.0.0.1"
-queue = Queue()
-open_ports = []
+def port_scan(host, port):
+	sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.settimeout(0.5)
+	try:
+		connect = sock.connect((host, port))
+		print("Port ", port, " openned")
+		connect.close()
+	except:
+		pass
 
-def portscan(port):
- try:
- 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
- 	sock.connect((target, port))
- 	return True
- except:
- 	return False
-
-def get_ports(mode):
- 	if mode == 1:
- 		for port in range(1, 1024):
- 			queue.put(port)
- 	elif mode == 2:
- 		for port in range(1, 49152):
- 			queue.put(port)
- 	elif mode == 3:
- 		ports = [20, 21, 22, 23, 25, 53, 80, 110, 443]
- 		for port in ports:
- 			queue.put(port)
- 	elif mode == 4:
- 		ports = input("Введите свой порт:")
- 		ports = ports.split()
- 		ports = list(map(int, ports))
- 		for port in ports:
- 	queue.put(port) 
-def worker():
-    while not queue.empty():
-        port = queue.get()
-        if portscan(port):
-            print("Порт {} открыт".format(port))
-            open_ports.append(port)
-        else:
-            print("Порт {} закрыт".format(port))
-
-def run_scanner(threads, mode):
-    get_ports(mode)
-    thread_list = []
-    for t in range(threads):
-        thread = threading.Thread(target=worker)
-        thread_list.append(thread)
-    for thread in thread_list:
-        thread.start()
-    for thread in thread_list:
-        thread.join()
-    print("Открытые порты - ", open_ports)
-
-run_scanner(100, 1)
+host="127.0.0.1"
+for i in range(1000):
+	port_scan(host, i)
