15dd048ab96da20dd9e1fed2cd9d448f4a9b3189
Vlad <vlad07020@mail.ru>
2019-10-27 13:51:25 +0300

    Zadanie 2

--- client.py
+++ client.py
@@ -1,10 +1,14 @@
-import socket 
-
-sock = socket.socket()
-
-sock.connect(('localhost', 8082))
-sock.send(b'hello, world!')
-data = sock.recv(1024)
-
-sock.close()
-print(data.decode())
+import socket
+host="127.0.0.1"
+port=8080
+client=socket.socket()
+client.connect((host, port))
+client.send("This is from client".encode())
+while True:
+	in_data = client.recv(1024)
+	print("From server: ", in_data.decode())
+	out_data = input()
+	client.send(out_data.encode())
+	if out_data=="buy":
+		break
+client.close()
