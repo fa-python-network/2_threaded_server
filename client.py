10f166022c9002a655db9759a5064296f8815dee
Сергей Ефремов <yefr@MacBook-Pro-Sergej-3.local>
2019-10-28 21:12:47 +0300

    Main task

--- client.py
+++ client.py
@@ -1,16 +1,77 @@
 import socket
-from time import sleep
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+host = input('Enter host name: ')
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+if host == 'localhost':
 
-data = sock.recv(1024)
+    pass
+
+else:
+
+    if any(c.isalpha() for c in host) == True:
+
+        print('Incorrect host name entered. The local host is selected, by default')
+
+        host = 'localhost'
+
+    else:
+
+        host_lst = host.split('.')           
+
+        for i in host_lst:
+
+            if 0 <= int(i) <= 255:
+
+                pass
+
+            else:
+                host = 'localhost'
+                print('Incorrect host name entered. The local host is selected, by default')
+
+try:
+
+    port = int(input('Enter port name: '))
+
+
+    if 0 <= port <= 65535:
+        pass
+
+    else:
+
+        print('Incorrect port name entered. Номер порта по умолчанию 9089')
+
+
+        port = 9089
+
+except ValueError:
+
+    print("Incorrect port name entered. The default port number is 9089")
+
+
+    port = 9089  
+
+sock.connect((host, port))
+
+print('Type exit to disconnect from the server')
+
+msg = ''
+
+while True:
+
+
+    if msg != 'exit':
+
+        print('Enter the message:')
+
+        msg = input()
+
+        sock.send(msg.encode())
+
+        data = sock.recv(1024)
+    else:
+
+        break
 
 sock.close()
-
-print(data.decode())
+print('Server operation completed.') 
--- server.py
+++ server.py
@@ -1,20 +1,43 @@
 import socket
+import threading
+
+def client(conn, addr):
+    print('A new thread has launched')
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+
+        print(data.decode())
+        conn.send(data)
 
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
 
-msg = ''
+port = 9089
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    try:
 
-print(msg)
+        sock.bind(('', port))
+        break
 
-conn.close()
+    except:
+        print(f'The port is busy {port}')
+
+        port += 1
+
+print (f'Server joined the port: {port}')
+
+sock.listen(3)
+
+while True:
+    conn, addr = sock.accept()
+
+    print(f'Client {addr} has joined the server')
+
+    p = threading.Thread(target = client, args = (conn, addr))
+
+    p.start()
+
+sock.close()
+print('Server has shut down')
