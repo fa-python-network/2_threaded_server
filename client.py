1b1fc58091513dfd7848f8a203c621c21a0a1a71
daria143 <55450503+daria143@users.noreply.github.com>
2019-10-28 23:43:14 +0300

    Полынцова ПИ18-4

--- client.py
+++ client.py
@@ -1,16 +1,45 @@
 import socket
-from time import sleep
+
+while True:
+    port = input('Введите порт от 1024 до 65535: \n')
+    if not port.isnumeric():
+        print('Ошибка')
+    elif 1023 <= int(port) <= 65535:
+        break
+    else:
+        print('Ошибка: порт не входит в нужный диапазон')
+
+while True:
+    address = input('Введите ip сервера или оставьте пустым для значения localhost: \n')
+    if address == '':
+        address = 'localhost'
+        break
+    else:
+        break
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.setblocking(True)
+sock.connect((address, int(port)))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+auth = sock.recv(1024).decode()
+print(auth)
+if auth[0] == 'I':
+    sock.send(input().encode())
+    print(sock.recv(1024).decode())
+    sock.send(input().encode())
+else:
+    print(sock.recv(1024).decode())
+while True:
+    sock.send(input().encode())
+    answer = sock.recv(1024).decode()
+    print(answer)
+    if answer[0] == 'C':
+        break
 
-data = sock.recv(1024)
-
-sock.close()
-
-print(data.decode())
+while True:
+    mesg = input("Input message: ")
+    if mesg == "exit":
+        sock.close()
+        break
+    sock.send(mesg.encode())
+    data = sock.recv(1024)
