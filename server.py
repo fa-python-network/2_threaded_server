44c0e172bd155c05055708bf79c9c7e41b1396e8
nastyapl77 <55870686+nastyapl77@users.noreply.github.com>
2019-11-01 13:32:01 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,52 @@
-import socket
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  1 09:21:44 2019
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+@author: 184136
+"""
 
-msg = ''
+from socket import socket
+from threading import Thread
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
 
-print(msg)
+def main():
+    def handle_client(conn, addr):
+        data = b''
+        while True:
+            chunk = conn.recv(1024)
+            data += chunk
+            if len(chunk) < 1024:
+                break
 
-conn.close()
+        text = data.decode()
+        print(f'Sever have got "{text}" from {addr}')
+
+        conn.send(data)
+        print(f'{data} sent back to client')
+
+        conn.close()
+        print(f'Connection with {addr} finished')
+
+    while True:
+        conn, addr = sock.accept()
+        addr = addr[0]
+        print(f'New client "{addr}" connected')
+
+        new_client_handler = Thread(target=handle_client, args=(conn, addr))
+        new_client_handler.start()
+
+
+sock = socket()
+print('Server started here')
+
+host, port = '', 8000
+
+sock.bind((host, port))
+sock.listen(5)
+print('Port listening started')
+
+main()
+
+sock.close()
+
+print('Server stopped')
