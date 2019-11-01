0c653e32e2871b6bdddfb7f62dddbc5fa50d35c5
nastyapl77 <55870686+nastyapl77@users.noreply.github.com>
2019-11-01 13:31:15 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,46 @@
-import socket
-from time import sleep
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  1 09:20:29 2019
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+@author: 184136
+"""
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+from socket import socket
 
-data = sock.recv(1024)
+
+sock = socket()
+host, port = 'localhost', 8000
+connected = False
+
+try:
+    sock.connect((host, port))
+    connected = True
+except ConnectionError:
+    print("Unreachable server")
+
+if connected:
+    print(f"Connected to {host}:{port}")
+
+    msg = input('Enter your message: ')
+    sent = False
+    try:
+        sock.send(msg.encode())
+        sent = True
+    except ConnectionError:
+        print('Sending error')
+
+    if sent:
+        print(f'Message "{msg}" sent to {host}:{port}')
+
+        answer = b''
+        while True:
+            chunk = sock.recv(1024)
+            answer += chunk
+            if not chunk:
+                break
+
+        print(f'"{answer.decode()}" returned by server')
 
 sock.close()
 
-print(data.decode())
+input('Press Enter to exit...')
