5b74bb6599788ee0cad9c275eed927db21e7e312
Глеб Шашин <65556037+shamoro@users.noreply.github.com>
2020-05-21 16:17:52 +0300

    Сканер портов

--- scanner.py
+++ scanner.py
@@ -1,15 +1,41 @@
+import threading
 import socket
-from threading import Thread
+
 
 N = 2**16 - 1
 
-for port in range(1,100):
-    sock = socket.socket()
-    try:
-        print(port)
-        sock.connect(('127.0.0.1', port))
-        print("Порт", i, "открыт")
-    except:
-        continue
-    finally:
-        sock.close()
+def portscan(a):
+
+    for n in range(a-1000, a):
+        if n >=1:
+            sock = socket.socket()
+            sock.settimeout(1)
+            try:
+                sock.connect(('127.0.0.1', n))
+                print("Порт :", n, "открыт")
+            except:
+                
+                    sock.close()
+
+                    pass
+
+
+port_set = []
+c = 0
+b = 0
+
+while True:
+    if N >= b:
+        b += 1000
+        port_set.append(b)
+        c += 1
+    else:
+        port_set[c-1] - 1000 + N % 1000
+        break
+                       
+                              
+
+print(port_set)
+                      
+thr = [threading.Thread(target=portscan, args=[a]) for a in port_set]
+[t.start() for t in thr]
