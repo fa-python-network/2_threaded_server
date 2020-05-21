e49fb6accfd531e60b274d1c0ac8e75433a15699
RoyalGoose <velesn@ya.ru>
2020-05-21 12:39:57 +0300

    making threads

--- scanner.py
+++ scanner.py
@@ -1,15 +1,38 @@
 import socket
-from threading import Thread
+import threading
 
 N = 2**16 - 1
 
-for port in range(1,100):
-    sock = socket.socket()
-    try:
-        print(port)
-        sock.connect(('127.0.0.1', port))
-        print("Порт", port, "открыт")
-    except:
-        continue
-    finally:
-        sock.close()
+
+def scanning(n):
+    print("Начат поток", n)
+    for port in range(n-1000, n):
+        if port >= 1:
+            sock = socket.socket()
+            try:
+                print("Проверка порта ", port)
+                sock.connect(('127.0.0.1', port))
+                print("Порт", port, "открыт")
+            except:
+                continue
+            finally:
+                sock.close()
+    print("Закрыт поток ", n)
+
+
+port_list = []
+i = 0
+m = 0
+while True:
+    if m <= N:
+        m += 1000
+        port_list.append(m)
+        i += 1
+    else:
+        port_list[i-1] = port_list[i-1] - 1000 + N % 1000
+        break
+
+print(port_list)
+threads = [threading.Thread(target=scanning, args=[n]) for n in port_list]
+[t.start() for t in threads]
+
