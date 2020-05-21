f92ae5638f0030ff5e143ca359244dbc8e865391
RoyalGoose <velesn@ya.ru>
2020-05-22 00:19:23 +0300

    threaded server & scanner

--- client.py
+++ client.py
@@ -1,16 +1,44 @@
 import socket
-from time import sleep
+import threading
+import datetime
 
+lock = threading.Lock()
+
+
+def send_interface():
+    global NICKNAME
+    msg = ''
+    print("===\n"
+          "For closing the client type 'close'\n"
+          "===")
+    while True:
+        time = datetime.datetime.now().time()
+        msg = str(input("Message:\n"))
+        if msg != 'close':
+            output_data = str(time.strftime("%H:%M:%S ")) + NICKNAME + ": " + msg
+            sock.send(output_data.encode())
+        else:
+            sock.close()
+            print("You closed the chat")
+            break
+
+
+def receive_interface():
+    while True:
+        lock.acquire()
+        data = sock.recv(1024)
+        if data.decode() == 'bye':
+            break
+        print(data.decode())
+        if not data:
+            break
+        lock.release()
+
+
+NICKNAME = input("Enter your nickname:\n")
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.connect(('localhost', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
-
-sock.close()
-
-print(data.decode())
+threading.Thread(target=send_interface, daemon=False).start()
+threading.Thread(target=receive_interface, daemon=False).start()
--- server.py
+++ server.py
@@ -1,20 +1,96 @@
 import socket
+import threading
+import sys
 
+CHAT_VIEWER = True
+SERVER_IS_ON = True
+SOCK_LISTENING = True
+BROADCAST_MESSAGE = b'online'
+CLIENTS_LIST = []
+
+
+class AdminThread(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.daemon = True
+
+	def run(self):
+		global CHAT_VIEWER
+		global SERVER_IS_ON
+		global SOCK_LISTENING
+		while True:
+			admin_panel = ("===\n"
+				  "Type 'chat' for chat viewing\n"
+				  "Type 'pause' to pause port listening\n"
+				  "Type 'shutdown' for stopping server\n"
+				  "===")
+			print(admin_panel)
+			command = input("Command:\n")
+			if command == 'chat':
+				if CHAT_VIEWER:
+					CHAT_VIEWER = False
+					print("Chat view:", CHAT_VIEWER)
+				else:
+					CHAT_VIEWER = True
+					print("Chat view:", CHAT_VIEWER)
+			elif command == 'pause':
+				if SOCK_LISTENING:
+					SOCK_LISTENING = False
+					print("Port listening:", SOCK_LISTENING)
+				else:
+					SOCK_LISTENING = True
+					print("Port listening:", SOCK_LISTENING)
+			elif command == 'shutdown':
+				SERVER_IS_ON = False
+				print("Shutting down")
+				sys.exit(1)
+			else:
+				print("Unknown command, enter a correct one")
+				print(admin_panel)
+
+
+class ClientThread(threading.Thread):
+	def __init__(self, conn, addr):
+		threading.Thread.__init__(self)
+		self.daemon = True
+		self.conn = conn
+		self.addr = addr
+
+	def run(self):
+		global BROADCAST_MESSAGE
+		global CLIENTS_LIST
+		text = "Connection from %s:%s" % (self.addr[0], self.addr[1])
+		print(text)
+		try:
+			while True:
+				if not SERVER_IS_ON:
+					print("Trying to exit")
+					break
+				data = self.conn.recv(1024)
+				if not data:
+					break
+				msg = data.decode()
+				BROADCAST_MESSAGE = data
+				for client in CLIENTS_LIST:
+					client.send(BROADCAST_MESSAGE)
+				if CHAT_VIEWER:
+					print(msg)
+		except:
+			client_socket = "%s:%s" % (self.addr[0], self.addr[1])
+			print("Client", client_socket, "disconnected")
+		self.conn.close()
+		CLIENTS_LIST.remove(self.conn)
+
+
+AdminThread().start()
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
+sock.bind(('localhost', 9090))
 
 while True:
-	data = conn.recv(1024)
-	if not data:
+	if not SERVER_IS_ON:
+		print("Trying to exit")
 		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
-
-conn.close()
+	sock.listen(0)
+	conn, addr = sock.accept()
+	CLIENTS_LIST.append(conn)
+	ClientThread(conn, addr).start()
