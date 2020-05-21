171e01b5550f5518ab79c1ab9f377688a7b0ec1e
Anna <govorova.anna97@mail.ru>
2020-05-21 10:05:13 +0000

    threaded scanner

--- scanner.py
+++ scanner.py
@@ -2,14 +2,19 @@ import socket
 from threading import Thread
 
 N = 2**16 - 1
+count_n = N//1000+1
 
-for port in range(1,100):
-    sock = socket.socket()
-    try:
-        print(port)
-        sock.connect(('127.0.0.1', port))
-        print("Порт", i, "открыт")
-    except:
-        continue
-    finally:
-        sock.close()
+def proc(n):  
+    for port in range(1000*n-999,1000*n):
+        sock = socket.socket()
+        try:
+            #print(port)
+            sock.connect(('127.0.0.1', port))
+            print("Порт", port, "открыт")
+        except:
+            continue
+        finally:
+            sock.close()
+
+threads = [Thread(target = proc, args=[i+1]) for i in range(count_n)]
+[t.start() for t in threads]
