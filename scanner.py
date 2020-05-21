d1263e737ca71749c1136ed99582c96dc3ac7016
Denistopalu <denistopalu@mail.ru>
2020-05-21 14:17:18 +0300

    Сканер портов

--- scanner.py
+++ scanner.py
@@ -1,15 +1,30 @@
+# coding=utf-8
+import threading
 import socket
-from threading import Thread
 
-N = 2**16 - 1
 
-for port in range(1,100):
-    sock = socket.socket()
-    try:
-        print(port)
-        sock.connect(('127.0.0.1', port))
-        print("Порт", i, "открыт")
-    except:
-        continue
-    finally:
-        sock.close()
+def portscan(port):
+    global r
+    d = port * 1024
+    f = d - 1024
+    for i in range(f, d):
+            sock = socket.socket()
+            sock.settimeout(0.5)
+            if i == r-1:
+                print ("Сканирование завершено!")
+            try:
+                    connection = sock.connect(('127.0.0.1', i))
+                    print('Порт :', i, "открыт.")
+                    connection.close()
+            except:
+                # print(i)
+                pass
+
+
+# Введите число s от 1 до 64 (это число умножается на 1024) для проверки портов. 1 = 1024, 64 = 65536
+s = 4
+r = s * 1024
+print ("Запущено сканирование " + str(r) + " портов")
+for element in range(s + 1):
+    t = threading.Thread(target=portscan, kwargs={'port': element})
+    t.start()
