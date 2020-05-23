d26b2871b7e7aaf5bd81f301a11d81c2439268b9
Lomovtseva Ekaterina <elomovtseva.2000@mail.ru>
2020-05-23 15:15:56 +0300

    test

--- client.py
+++ client.py
@@ -2,15 +2,36 @@ import socket
 from time import sleep
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+host = input('Введите имя хоста:')
+if host == 'localhost':
+    pass
+else:
+    if any(c.isalpha() for c in host) == True:
+        print('некорректное имя хоста.По умолчанию локальный хост')
+        host = 'localhost'
+    else:
+        host_lst = host.split('.')
+        for i in host_lst:
+            if 0 <= int(i) <= 255:
+                pass
+            else:
+                host = 'localhost'
+                print('Некорректное имя хоста.По умолчанию локальный хост')
 
-data = sock.recv(1024)
+try:
+    port = int(input('номер порта:'))
+    if 0 <= port <= 65535:
+        pass
+    else:
+        print('некорректный номер порта. порт по умолчанию 9090')
+        port = 9090
 
-sock.close()
+except ValueError:
+    print("Некорректный номер порта. по умолчанию 9090")
+    port = 9090
 
-print(data.decode())
+sock.connect((host, port))
+
+print('exit для завершения работы с сервером')
+msg = ''
--- server.py
+++ server.py
@@ -1,20 +1,70 @@
-import socket
+﻿import socket
+import json
+import threading
+
+f = open('log.txt', 'w')
 
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+port = 9090
+while True:
+    try:
+        if port == 65536:
+            print('Все порты заняты')
+            break
+        sock.bind(('', port))
+        break
+    except:
+        port += 1
 
-msg = ''
+print(f'Port number: {port}')
+print("Server starts", file=f)
+sock.listen(0)
+print("Now listen", file=f)
+
+
+def work(conn, addr):
+    with open('data.json', 'r+') as d:
+        data = json.loads(d.read())
+        for i in data['clients']:
+            if i['ip'] == addr[0]:
+                hello = f"Hello {i['name']}"
+                conn.send(hello.encode())
+                conn.send(b'Input your password:')
+                while True:
+                    password = conn.recv(1024).decode()
+                    if i['password'] == password:
+                        conn.send(b'Correct password!')
+                        break
+                    else:
+                        conn.send(b'Wrong password! Try another')
+                break
+        else:
+
+            conn.send(b'Input your name:')
+            name = conn.recv(1024).decode()
+            conn.send(b'Input your password:')
+            password = conn.recv(1024).decode()
+            client = {"ip": addr[0], "name": name, "password": password}
+            data['clients'].append(client)
+            d.seek(0)
+            d.write(json.dumps(data))
+
+    msg = ''
+    while True:
+        data = conn.recv(1024)
+        print("new data from client", file=f)
+        if not data:
+            break
+        msg = msg + data.decode() + ' '
+        conn.send(data)
+
+    print("data to client", file=f)
+    print(f'Сообщение от пользователя: "{msg}"')
+    conn.close()
+    print('stop client', file=f)
+
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
-
-conn.close()
+    conn, addr = sock.accept()
+    thr = threading.Thread(target=work, args=[conn, addr])
+    thr.start()
