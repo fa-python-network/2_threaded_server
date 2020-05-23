5191f4b452f2008cbf8116966b8245f42f9ed69f
“dronio328” <dronio328@gmail.com>
2020-05-23 15:40:27 +0300

    Андрей Абу ЗБ ПИ-2-1 Задания 1-3

--- client.py
+++ client.py
@@ -1,16 +1,47 @@
 import socket
+import threading
 from time import sleep
+import sys
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port=int(input('Please enter your port number from 1024 to 65535: '))
+if not (port >= 1024 and port <= 65535):
+    port=9091
+sock.connect(('localhost',port))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
 
-data = sock.recv(1024)
+class Client():
+    def __init__(self, sock, port):
+        threading.Thread.__init__(self)
+        self.sock = sock
+        self.port = port
 
-sock.close()
+        self.username = input('Please enter your username: ')
+        self.thread_cl()
 
-print(data.decode())
+    def thread_cl(self):
+        threading.Thread(target = self.recv_msg).start() #Threaded
+        HEADERSIZE = 10
+        while True:
+
+            msg = input(f"Enter your message, dear {self.username} : ")
+            #msg = f"{len(msg):<{HEADERSIZE}}" + msg
+            if msg == 'exit' :
+                self.sock.close()
+                break
+            else:
+                self.sock.send(msg.encode())
+        self.sock.close()
+
+    def recv_msg(self):
+
+#Threaded
+
+        while True:
+            self.data = self.sock.recv(1024)
+            print(self.data.decode())
+        if not self.data:
+            print('Disconnect')
+            sys.exit(0)
+
+Cl = Client(sock,port)
--- server.py
+++ server.py
@@ -1,20 +1,47 @@
 import socket
+import sys
+import threading
+import pickle
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
 
-msg = ''
+class Server(threading.Thread):
+    def __init__(self, clients = []):
+        super().__init__(daemon = True)
+        self.clients = clients
+        self.port = int(input("Please enter your port from 1024 to 65535: "))
+        self.sock = None
+        self.work_serv()
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    def work_serv(self):
+        self.sock = socket.socket()
+        while True:
+            try:
+                self.sock.bind(('',self.port))
+                break
+            except:
+                self.port += 1
+        print(f'Got your port  {self.port}')
+        self.sock.listen(5)
+        HEADERSIZE = 10
 
-print(msg)
+        while True:
+            conn, addr = self.sock.accept()
+            print(f"Connection from client with IP {addr[0]} has been established")
+            threading.Thread(target = self.conn_client,args = (conn,addr)).start()
+            msg = f"Welcome to the server,{addr}!"
+            msg = f"{len(msg):<{HEADERSIZE}}" + msg
+            conn.send(bytes(msg,"utf-8"))
+            self.clients.append(conn)
 
-conn.close()
+    def conn_client(self,conn,addr):
+#Connection to the usr
+        while True:
+            data = conn.recv(1024).decode()
+            if not data:
+                conn.close()
+                break
+            else:
+                print(f"Message: {data} is from {addr}")
+
+
+serv=Server()
