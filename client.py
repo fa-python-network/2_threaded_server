9bf536dcbb0d9871826405f8e8fd4b6b4fbfccc4
Mikael Arutyunyan <65557368+7mika7@users.noreply.github.com>
2020-05-23 14:06:46 +0300

    Mikael Arutyunyan

--- client.py
+++ client.py
@@ -1,16 +1,12 @@
 import socket
-from time import sleep
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('localhost', 9999))
+client.sendall(bytes("This is from Client", 'UTF-8'))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+while True:
+    in_data = client.recv(1024)
+    print("From Server :", in_data.decode())
+    out_data = input()
+    client.sendall(bytes(out_data, 'UTF-8'))
 
-data = sock.recv(1024)
-
-sock.close()
-
-print(data.decode())
--- server.py
+++ server.py
@@ -1,20 +1,30 @@
 import socket
+import threading
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
 
-msg = ''
+class ClientThread(threading.Thread):
+    def __init__(self, clientAddress, clientsocket):
+        threading.Thread.__init__(self)
+        self.csocket = clientsocket
 
+    def run(self):
+        print ("Connection from : ", clientAddress)
+        self.csocket.send(bytes("Hi, This is from Server..", 'utf-8'))
+        msg = ''
+        while True:
+            data = self.csocket.recv(2048)
+            msg = data.decode()
+            print ("from client", msg)
+            self.csocket.send(bytes(msg, 'UTF-8'))
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind(('localhost', 9999))
+print("Server started")
+print("Waiting for client request..")
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
-
-conn.close()
+    server.listen(1)
+    clientsock, clientAddress = server.accept()
+    newthread = ClientThread(clientAddress, clientsock)
+    newthread.start()
