30e76cc6db755cba1f7c1aeca1cbfe6a7ce2f59b
Ferllord <114143368+Ferllord@users.noreply.github.com>
2022-11-14 21:01:13 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,14 @@
 import socket
 from time import sleep
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+with socket.socket() as sock:
+  sock.setblocking(1)
+  sock.connect(('10.38.165.12', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+  #msg = input()
+  msg = "Hi!"
+  sock.send(msg.encode())
 
-data = sock.recv(1024)
-
-sock.close()
+  data = sock.recv(1024)
 
 print(data.decode())
